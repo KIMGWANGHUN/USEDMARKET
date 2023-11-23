213686e0a63bb721d0c53e28c85ace836b55a000
twzt9983@naver.com <twzt9983@naver.com>
2023-11-23 22:35:03 +0900

    20231123_OJH

---
+++ src/main/java/com/project/board/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.project.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = {"/"})
+public class UserController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/UserJoinPage")
+    public ModelAndView userJoinPage() {
+        ModelAndView mav = new ModelAndView("/UserJoinPage");
+        return mav;
+    }
+}
