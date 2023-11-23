abf2069e2b9a16ae80a7499a7abc1595727a03df
KGH <violetray26@naver.com>
2023-11-23 22:34:33 +0900

    1123_final

--- src/main/java/com/project/board/entity/Board.java
+++ src/main/java/com/project/board/entity/Board.java
@@ -7,13 +7,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 @Data
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String title;
-    private String content;
+    private Integer bId;
+    private String bTitle;
+    private String bCategory;
+    private String bContent;
+    private String bPrice;
+    private String bImage;
+    private String bAddress;
+    private Date bDate;
+    private int bViews;
 }
