b176491735342a19323c610858b15704c391edfd
KIMGWANGHUN <violetray26@naver.com>
2023-11-26 22:33:25 +0900

    board:crud작업

--- src/main/java/com/project/board/controller/BoardController.java
+++ src/main/java/com/project/board/controller/BoardController.java
@@ -1,8 +1,6 @@
 package com.project.board.controller;
 
 import com.project.board.entity.Board;
-import com.project.board.entity.UserEntity;
-import com.project.board.repository.BoardRepository;
 import com.project.board.sevice.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
